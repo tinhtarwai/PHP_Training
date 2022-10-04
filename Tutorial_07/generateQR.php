d4479db9d3758850090bcff05aa0e7871cdc6bda
nweni <nweni2811ucst@gmail.com>
2022-10-04 21:26:00 +0630

    Tuto-7 update

--- Tutorial_07/generateQR.php
+++ Tutorial_07/generateQR.php
@@ -9,26 +9,23 @@
 </head>
 <body>
     <?php
-    include "phpqrcode/qrlib.php";
+    require_once 'phpqrcode/qrlib.php';
 
     if (isset($_POST['generate']) && !empty($_POST['name'])) {
         $path = 'QR_codes/';
         $file = $path . $_POST['name'] . '.png';
-        $input_text = $_POST['name'];
-        echo "<center><h2>Your QR Code</h2></center>";
-        if ($file !== '' && $input_text !== '') {
+        $name = $_POST['name'];
+        echo "<center><h2>Your QR Code is heare!</h2></center>";
+        QRcode::png($name, $file, 'L', 5, 2);
         ?>
-            <center><img src='QR_codes/<?php echo $_POST['name'] ?>.png'></center>
-        <?php
-        } else {
-            echo "<center>Name is required!</center>";
-        }
-        QRcode::png($input_text, $file);
+        <center><img src='QR_codes/<?php echo $_POST['name'] ?>.png'></center>
+        <center><h4><?php echo $_POST['name'] ?>.png</h4></center>
+        <a href = "index.php" class = "next">Create another QR code
+        <?php   
     } else {
         echo "<p class = warning>Please enter a name!</p>";
         header('Refresh:3; URL= index.php');
     }
-
 ?>
 </body>
 </html>
