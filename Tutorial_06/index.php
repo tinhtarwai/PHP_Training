d01426a8710ea28bf2dc468b26e06c37bb86a1dc
tinhtarwai <tinhtarwai106330@gmail.com>
2022-10-11 12:54:56 +0630

    index fixed

--- Tutorial_06/index.php
+++ Tutorial_06/index.php
@@ -18,39 +18,39 @@
 
         if(isset($_POST["upload"])) { 
             if(empty($folder)){
-            echo "<p class = fail>Warning! You have to fill a folder name.</p>";
-            header('Refresh: 3; URL = index.php');
+                echo "<p class = fail>Warning! You have to fill a folder name.</p>";
+                header('Refresh: 3; URL = index.php');
             } else {
             // Check if folder is already exit
-            if (!file_exists($folder)) {
-                mkdir($folder, 0777, true);
-            } 
-            // Check if file is selected
-            if(empty($_FILES['photo']['name'])) {
-                echo "<p class = fail>Warning! You have to choose an image</p>";
-            } else {
-                // Check if file already exists
-                if (file_exists($target_file)) {
-                echo "<p class = fail>File already exists.</p>";
-                $uploadOk = 0;
+                if (!file_exists($folder)) {
+                    mkdir($folder, 0777, true);
+                } 
+                // Check if file is selected
+                if (empty($_FILES['photo']['name'])) {
+                    echo "<p class = fail>Warning! You have to choose an image</p>";
                 } else {
-                    // check file formats
-                    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-                    && $imageFileType != "gif" ) {
-                    echo "<p class = fail>Only JPG, JPEG, PNG & GIF files are allowed.</p>";
-                    $uploadOk = 0;
+                    // Check if file already exists
+                    if (file_exists($target_file)) {
+                        echo "<p class = fail>File already exists.</p>";
+                        $uploadOk = 0;
+                    } else {
+                        // check file formats
+                        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                        && $imageFileType != "gif" ) {
+                            echo "<p class = fail>Only JPG, JPEG, PNG & GIF files are allowed.</p>";
+                            $uploadOk = 0;
+                        }
                     }
                 }
+            // Check if $uploadOk is 0 or 1
+                if ($uploadOk == 0) {
+                    echo "<p class = fail>Sorry, your file was not uploaded.</p>";
+                } else {
+                    if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
+                        echo "<p class = success>Congratulations! The file  \"". htmlspecialchars( basename( $_FILES["photo"]["name"])). "\"  has been uploaded to \"$folder\" folder.</p>";
+                    } 
+                }
             }
-        // Check if $uploadOk is 0 or 1
-        if ($uploadOk == 0) {
-            echo "<p class = fail>Sorry, your file was not uploaded.</p>";
-            } else {
-            if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
-                echo "<p class = success>Congratulations! The file  \"". htmlspecialchars( basename( $_FILES["photo"]["name"])). "\"  has been uploaded to \"$folder\" folder.</p>";
-            } 
-            }
-        }
         //header('Refresh: 4; URL = index.php');
         }
     ?>
--- Tutorial_07/index.php
+++ Tutorial_07/index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
-  <title>QR Code Generator</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>QR Code Generator</title>
 </head>
 <body>
-  <form action="generateQR.php" method= "post">
-    <h3>Make Your Own QR Code</h3>
-    <!--<label for="name">Enter Name: </label> -->
-    <input type="text" name= "name" placeholder = "Enter Your Desire Name"><br>
-    <input type="submit" value="Generate QR Code" name= "generate" class= "btn">
-  </form>
+    <form action="generateQR.php" method= "post">
+        <h3>Make Your Own QR Code</h3>
+        <!--<label for="name">Enter Name: </label> -->
+        <input type="text" name= "name" placeholder = "Enter Your Desire Name"><br>
+        <input type="submit" value="Generate QR Code" name= "generate" class= "btn">
+    </form>
 </body>
 </html>
