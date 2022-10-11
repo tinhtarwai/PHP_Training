151def2ca458c521c84fc7cf7cf955d9b33136e8
tinhtarwai <tinhtarwai106330@gmail.com>
2022-10-11 12:57:32 +0630

    index fixed

--- Tutorial_08/index.php
+++ Tutorial_08/index.php
@@ -5,11 +5,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
-  <title>CRUD Tuto</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>CRUD Tuto</title>
 </head>
 <body>
     <div class= "div">
@@ -17,15 +17,15 @@
         <button  class="cmn-btn add"><a href="user-add.php"> Add new user</a></button>
     </div>
     <table>
-      <tr>
-        <th>User ID</th>
-        <th>User Name</th>
-        <th>Age</th>
-        <th>Gender</th>
-        <th>Phone No</th>
-        <th>Address</th>
-        <th colspan="2">Operations</th>
-      </tr>
+        <tr>
+            <th>User ID</th>
+            <th>User Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>Phone No</th>
+            <th>Address</th>
+            <th colspan="2">Operations</th>
+        </tr>
     <?php
     $sql = "SELECT * FROM users;";
     $result = mysqli_query($conn, $sql);
@@ -47,8 +47,8 @@
                 <td>'.$phone.'</td>
                 <td>'.$address.'</td>
                 <td>
-                <button class= "cmn-btn edit"><a href= "user-edit.php?updateid='.$id.'">Edit</a></button>
-                <button class= "cmn-btn delete"><a href= "user-delete.php?deleteid='.$id.'">Delete</a></button>
+                    <button class= "cmn-btn edit"><a href= "user-edit.php?updateid='.$id.'">Edit</a></button>
+                    <button class= "cmn-btn delete"><a href= "user-delete.php?deleteid='.$id.'">Delete</a></button>
                 </td>
             </tr>
             ';
